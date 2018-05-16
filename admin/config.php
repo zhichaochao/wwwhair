4ee6696371ccf4f5690420e1677269ba2bcd4d2c
zhichaochao <928583371@qq.com>
2018-05-16 15:56:16 +0800

    首次上传

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.wp.com/admin/');
+define('HTTP_CATALOG', 'http://www.wp.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.wp.com/admin/');
+define('HTTPS_CATALOG', 'http://www.wp.com/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/phpStudy/WWW/w/admin/');
+define('DIR_SYSTEM', 'D:/phpStudy/WWW/w/system/');
+define('DIR_IMAGE', 'D:/phpStudy/WWW/w/image/');
+define('DIR_LANGUAGE', 'D:/phpStudy/WWW/w/admin/language/');
+define('DIR_TEMPLATE', 'D:/phpStudy/WWW/w/admin/view/template/');
+define('DIR_CONFIG', 'D:/phpStudy/WWW/w/system/config/');
+define('DIR_CACHE', 'D:/phpStudy/WWW/w/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/phpStudy/WWW/w/system/storage/download/');
+define('DIR_LOGS', 'D:/phpStudy/WWW/w/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/phpStudy/WWW/w/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/phpStudy/WWW/w/system/storage/upload/');
+define('DIR_CATALOG', 'D:/phpStudy/WWW/w/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'h');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.wp.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.wp.com/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/phpStudy/WWW/w/catalog/');
+define('DIR_SYSTEM', 'D:/phpStudy/WWW/w/system/');
+define('DIR_IMAGE', 'D:/phpStudy/WWW/w/image/');
+define('DIR_LANGUAGE', 'D:/phpStudy/WWW/w/catalog/language/');
+define('DIR_TEMPLATE', 'D:/phpStudy/WWW/w/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/phpStudy/WWW/w/system/config/');
+define('DIR_CACHE', 'D:/phpStudy/WWW/w/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/phpStudy/WWW/w/system/storage/download/');
+define('DIR_LOGS', 'D:/phpStudy/WWW/w/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/phpStudy/WWW/w/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/phpStudy/WWW/w/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'h');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
